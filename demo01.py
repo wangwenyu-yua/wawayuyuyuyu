a43abb1d947f1780e5dff34b8b5aa45bfdc98a80
wangwenyu-yua <68528830@qq.com>
2022-06-12 11:08:20 +0800

    6.12day

---
+++ demo01.py
@@ -0,0 +1,58 @@
+# 1.1
+#数据格式的转换
+
+#a=input()
+#b=input()
+#print("input获取的内容",a+b)
+
+# print(type("哈哈"))
+# print(type(23333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a=str(2333)
+# print(type(a))
+
+
+# 字符串/str
+# 整数/int
+# 小数/float
+# 布尔值/bool  True or  False
+# 元组/tuple
+# 数组/list
+# 字典/dict
+# 空 /NoneType   None
+
+
+
+
+# 2:任何数据都可以转换为字符串
+# 整数和小数可以互相转换
+# 字符串转换为其他的数据必须满足长得像这个规律
+
+# a=float (input("请输入"))
+# b=float (input("请输入"))
+# print("input获取的内容",a+b)
+
+
+# a='fhedojkpfjirhejkowdplokfjik   kjndoj'
+# print(len(a))
+
+
+'''
+通过代码获取不同的两段内容并且计算他们长度的和
+
+
+'''
+# a='fhedojkpfjirhejkowdplokfjik   kjndoj'
+# b='fhsjjfsdkhfsdjk'
+# print(len(a+b))
+
+
+a=input("请输入")
+b=input("请输入")
+print("两段字符的长度是：",len(a)+len(b))
+
---
+++ from unicodedata import name.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+
+
+a="张三"#把张三这个值赋值给了名字叫a的这个变量
+'''变量与赋值
+变量必须为小写字母
+=是赋值的作用'''
+
+name='王文宇'
+ print=（name）
